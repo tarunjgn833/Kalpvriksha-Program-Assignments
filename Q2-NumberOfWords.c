3e3f06a55c38effd74710db97c0fe6d948751764
Tarun Sharma <tarunjgn833@example.com>
2025-01-15 16:10:34 +0530

    Program counts number of words in a string, separated by space/s

---
+++ Q2-NumberOfWords.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_INPUT_LEN 1000
+
+int countWords(char *string)
+{
+    int lastWasWord = 0;
+    int wordCount = 0;
+
+    for (int i = 0; string[i] != '\0'; i++)
+    {
+
+        if (string[i] != ' ')
+        {
+            if (!lastWasWord)
+                wordCount++;
+            lastWasWord = 1;
+        }
+        else
+        {
+            lastWasWord = 0;
+        }
+    }
+    return wordCount;
+}
+
+int main()
+{
+    char input[MAX_INPUT_LEN];
+
+    fgets(input, sizeof(input), stdin);
+    input[strcspn(input, "\n")] = '\0';
+
+    printf("%d", countWords(input));
+    return 0;
+}
