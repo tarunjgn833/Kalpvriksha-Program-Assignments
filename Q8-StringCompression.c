6d9d491eb092c47154514660a7b37c39009c3640
Tarun Sharma <tarunjgn833@example.com>
2025-01-15 17:54:05 +0530

    Program compresses string from consecutive occurences of characters

---
+++ Q8-StringCompression.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+void compressString(char *input, char *output)
+{
+    int len = strlen(input);
+    int count = 1;
+    int j = 0;
+
+    for (int i = 0; i < len; i++)
+    {
+        if (input[i] == input[i + 1])
+        {
+            count++;
+        }
+        else
+        {
+            output[j++] = input[i];
+            if (count > 1)
+            {
+                output[j++] = count + '0';
+            }
+            count = 1;
+        }
+    }
+    output[j] = '\0';
+}
+
+int main()
+{
+    char input[1000];
+    char compressed[1000];
+
+    fgets(input, sizeof(input), stdin);
+    input[strcspn(input, "\n")] = '\0';
+
+    compressString(input, compressed);
+
+    if (strlen(compressed) < strlen(input))
+    {
+        printf("%s\n", compressed);
+    }
+    else
+    {
+        printf("%s\n", input);
+    }
+
+    return 0;
+}
